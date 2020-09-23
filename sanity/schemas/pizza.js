2b1c9f816fb0e6c038da3edb2d19295a250804dc
mcgraths7 <mcgrath.steven7@gmail.com>
2020-09-23 18:43:59 -0400

    added custom price input to show price in normal currency

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,3 +1,10 @@
+import PriceInput from '../components/PriceInput';
+
+const formatMoney = Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+}).format;
+
 export default {
   name: 'pizza',
   title: 'pizzas',
@@ -14,6 +21,7 @@ export default {
       name: 'price',
       title: 'Pizza Price',
       type: 'number',
+      inputComponent: PriceInput,
       description: 'How much does it cost in cents?',
       validation: (Rule) => Rule.min(1000).max(50000),
       // TODO: Add custom input component
@@ -46,22 +54,31 @@ export default {
     select: {
       title: 'name',
       media: 'image',
-      topping0: 'toppings.0.name',
-      topping1: 'toppings.1.name',
-      topping2: 'toppings.2.name',
-      topping3: 'toppings.3.name',
+      price: 'price',
+      toppingName0: 'toppings.0.name',
+      toppingName1: 'toppings.1.name',
+      toppingName2: 'toppings.2.name',
+      toppingName3: 'toppings.3.name',
+      toppingVeg0: 'toppings.0.vegetarian',
+      toppingVeg1: 'toppings.1.vegetarian',
+      toppingVeg2: 'toppings.2.vegetarian',
+      toppingVeg3: 'toppings.3.vegetarian',
     },
-    prepare: ({ title, media, ...toppings }) => {
-      // Filter undefined toppings out
-      // return preview object
-      // const filteredToppings = Object.values(toppings).filter(
-      //   (topping) => topping !== undefined
-      // );
-      const filteredTops = Object.values(toppings).filter(Boolean);
+    prepare: ({ title, media, price, ...toppings }) => {
+      const filteredTopNames = Object.values(toppings)
+        .slice(0, 3)
+        .filter(Boolean);
+      const filteredVeg = Object.values(toppings)
+        .slice(4, 7)
+        .filter((v) => v !== undefined);
+      const isVeg = filteredVeg.every((v) => v === true);
+      const modifiedTitle = isVeg ? `🌱 ${title}` : title;
+      const formattedPrice = price ? formatMoney(price / 100) : '$0.00';
+
       return {
-        title,
+        title: `${modifiedTitle} - ${formattedPrice}`,
         media,
-        subtitle: filteredTops.join(', '),
+        subtitle: filteredTopNames.join(', '),
       };
     },
   },
