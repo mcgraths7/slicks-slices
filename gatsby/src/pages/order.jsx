d948356f4fcb1d17e65d5099a69ae56117a0d8f9
mcgraths7 <mcgrath.steven7@gmail.com>
2020-09-30 15:23:03 -0400

    starting order form

--- gatsby/src/pages/order.jsx
+++ gatsby/src/pages/order.jsx
@@ -5,7 +5,14 @@ import SEO from '../components/SEO';
 const OrderPage = () => (
   <>
     <SEO title="Order Ahead" description="The orders page" />
-    <p>Order Ahead!</p>
+    <form>
+      <fieldset>
+        <legend>Your info</legend>
+      </fieldset>
+      <fieldset>
+        <legend>Menu</legend>
+      </fieldset>
+    </form>
   </>
 );
 
